77c65e51ed2278dc47e6fade349f1a38226de45b
markus <datamastery87@gmail.com>
2023-07-10 19:54:28 +0200

    react frotnend finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { FaSpinner } from "react-icons/fa";
 
 const App = () => {
   const [conversation, setConversation] = useState({ conversation: [] });
@@ -63,22 +64,35 @@ const App = () => {
   };
 
   return (
-    <div className="App flex flex-col items-center justify-center min-h-screen bg-gray-200">
-      <div className="flex flex-col p-4 bg-white rounded shadow w-full max-w-md space-y-4">
-        {conversation.conversation
-          .filter((message) => message.role !== "system")
-          .map((message, index) => (
-            <div
-              key={index}
-              className={`${
-                message.role === "user" ? "text-right" : "text-left"
-              }`}
-            >
-              <strong className="font-bold">{message.role}:</strong>
-              <span className="text-gray-700">{message.content}</span>
-            </div>
-          ))}
-      </div>
+    <div
+      className="App flex flex-col items-center pt-6 min-h-screen bg-gray-900 text-sm"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/images/background.jpg')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <h1 className="text-3xl font-bold mb-4 text-white">Restaurant Chatbot</h1>
+      {conversation.conversation.length > 0 && (
+        <div className="flex flex-col p-4 bg-white rounded shadow w-full max-w-md space-y-4">
+          {conversation.conversation
+            .filter((message) => message.role !== "system")
+            .map((message, index) => (
+              <div
+                key={index}
+                className={`${
+                  message.role === "user" ? "text-right" : "text-left"
+                }`}
+              >
+                <strong className="font-bold text-gray-900">
+                  {message.role}:
+                </strong>
+                <span className="text-gray-700">{message.content}</span>
+              </div>
+            ))}
+        </div>
+      )}
       <div className="flex flex-row w-full max-w-md mt-4">
         <input
           type="text"
@@ -91,22 +105,28 @@ const App = () => {
             }
           }}
           className="flex-grow mr-2 p-2 rounded border-gray-300"
+          placeholder={isLoading ? "Processing..." : "Type your message here"}
         />
         <button
           onClick={handleSubmit}
           disabled={isLoading}
-          className="p-2 rounded bg-blue-500 text-white"
+          className="p-2 rounded bg-blue-800 text-white"
         >
           Send
         </button>
       </div>
+      {isLoading && (
+        <div className="flex items-center space-x-4 text-l text-white">
+          <FaSpinner className="animate-spin" />
+          <span>Loading...</span>
+        </div>
+      )}
       <button
         onClick={handleNewSession}
-        className="mt-4 p-2 rounded bg-red-500 text-white"
+        className="mt-4 p-2 rounded bg-red-700 text-white absolute top-0 left-4"
       >
         New Session
       </button>
-      {isLoading && <div className="loader"></div>}
     </div>
   );
 };
